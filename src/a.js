7630d7c2b271dc12f24d94586530ea3231e45030
高强 <185559129@qq.com>
2020-04-10 12:07:10 +0800

    edit markdown

--- src/a.js
+++ src/a.js
@@ -1,7 +1,8 @@
 var bjs = require('./b.js')
 module.exports= {
     methoda:function () {
-        console.info('我是methoda111')
+        console.info('我是methoda11121')
+
         bjs.methodb()
     }
 }
